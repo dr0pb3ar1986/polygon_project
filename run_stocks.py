0a48e63251a6d6f132cf0b87dd58f361cfbf43db
dr0pb3ar1986 <dr0pb3ar1986@gmail.com>
2025-08-14 20:57:26 +0100

    First Commit

--- run_stocks.py
+++ run_stocks.py
@@ -15,6 +15,7 @@ def main():
         stocks_ticker_list.fetch_and_save_all_stock_tickers()
         stocks_ticker_overview.fetch_and_save_all_stock_overviews()
         stocks_ticker_relatives.fetch_and_save_all_stock_relatives()
+
         print("\n--- ✔️ All Stock Workflows Complete ---")
     except Exception as e:
         print(f"\n--- ❌ An error occurred during the stock workflows: {e} ---")
